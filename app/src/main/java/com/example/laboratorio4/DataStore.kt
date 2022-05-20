c13436cc6274a41ae34a7bf11c634f05e1ca6fe0
gelber12345 <gmartinezpas@unsa.edu.pe>
2022-05-20 18:37:50 -0500

    Avance

--- app/src/main/java/com/example/laboratorio4/DataStore.kt
+++ app/src/main/java/com/example/laboratorio4/DataStore.kt
@@ -3,9 +3,7 @@ package com.example.laboratorio4
 import android.content.Context
 import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -15,61 +13,105 @@ class Data(private val context: Context) {
 
     // to make sure there's only one instance
     companion object {
-        private val Context.dataStoree: DataStore<Preferences> by preferencesDataStore("userEmail")
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("userEmail")
         val USER_ESCUELA_KEY = stringPreferencesKey("user_escuela")
         val USER_PERIODO_KEY = stringPreferencesKey("user_periodo")
         val USER_CODIGO_KEY = stringPreferencesKey("user_codigo")
-        val USER_TEXTSIZE_KEY = stringPreferencesKey("user_textSize")
+        val USER_NOMBRE_KEY = stringPreferencesKey("user_nombre")
+        val USER_SEMESTRE_KEY = stringPreferencesKey("user_semestre")
+        val USER_DURACION_KEY = stringPreferencesKey("user_duracion")
+        val USER_TEXTSIZE_KEY = intPreferencesKey("user_size")
 
     }
 
 
-    //get the saved email
-    val getEscuela: Flow<String?> = context.dataStoree.data
+    //get the saved Escuela
+    val getEscuela: Flow<String?> = context.dataStore.data
         .map { preferences ->
             preferences[USER_ESCUELA_KEY] ?: "FirstLast@gmail.com"
         }
 
-    //save email into datastore
+    //save Escuela into datastore
     suspend fun saveEscuela(name: String) {
-        context.dataStoree.edit { preferences ->
+        context.dataStore.edit { preferences ->
             preferences[USER_ESCUELA_KEY] = name
         }
     }
-    //get the saved email
-    val getPeriodo: Flow<String?> = context.dataStoree.data
+    //get the saved Periodo
+    val getPeriodo: Flow<String?> = context.dataStore.data
         .map { preferences ->
             preferences[USER_PERIODO_KEY] ?: "FirstLast@gmail.com"
         }
 
-    //save email into datastore
+    //save Periodo into datastore
     suspend fun savePeriodo(name: String) {
-        context.dataStoree.edit { preferences ->
+        context.dataStore.edit { preferences ->
             preferences[USER_PERIODO_KEY] = name
         }
     }
-    //get the saved email
-    val getCodigo: Flow<String?> = context.dataStoree.data
+    //get the saved Codigo
+    val getCodigo: Flow<String?> = context.dataStore.data
         .map { preferences ->
             preferences[USER_CODIGO_KEY] ?: "FirstLast@gmail.com"
         }
 
-    //save email into datastore
+    //save Codigo into datastore
     suspend fun saveCodigo(name: String) {
-        context.dataStoree.edit { preferences ->
+        context.dataStore.edit { preferences ->
             preferences[USER_CODIGO_KEY] = name
         }
     }
-    //get the saved email
-    val getSize: Flow<Float?> = context.dataStoree.data
+    //get the saved Size
+    val getSize: Flow<Int> = context.dataStore.data
         .map { preferences ->
-            (preferences[USER_TEXTSIZE_KEY] ?: 12 as Float) as Float
+            // No type safety.
+            preferences[USER_TEXTSIZE_KEY] ?: 12
         }
-
-    //save email into datastore
-    suspend fun saveSize(data: Float) {
-        context.dataStoree.edit { preferences ->
-            preferences[USER_TEXTSIZE_KEY] = data.toString()
+    //save Size into datastore
+    suspend fun saveSize(data: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_TEXTSIZE_KEY] = data
         }
     }
+
+    //get the saved Nombre
+    val getNombre: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[USER_NOMBRE_KEY] ?: "FirstLast@gmail.com"
+        }
+
+    //save Nombre into datastore
+    suspend fun saveNombre(name: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_NOMBRE_KEY] = name
+        }
+    }
+
+    //get the saved Semestre
+    val getSemestre: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[USER_SEMESTRE_KEY] ?: "FirstLast@gmail.com"
+        }
+
+    //save Semestre into datastore
+    suspend fun saveSemestre(name: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_SEMESTRE_KEY] = name
+        }
+    }
+
+    //get the saved Duracion
+    val getDuracion: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[USER_DURACION_KEY] ?: "FirstLast@gmail.com"
+        }
+
+    //save Duracion into datastore
+    suspend fun saveDuracion(name: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_DURACION_KEY] = name
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/laboratorio4/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio4/MainActivity.kt
@@ -2,37 +2,48 @@ package com.example.laboratorio4
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import com.example.laboratorio4.ui.theme.Laboratorio4Theme
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
-lateinit var counterManager: Data
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Laboratorio4Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+
                     LoginSCreen()
-                }
+
             }
         }
     }
@@ -45,120 +56,73 @@ fun LoginSCreen() {
     val scope = rememberCoroutineScope()
     // we instantiate the saveEmail class
     val dataStore = Data(context)
-    val textSize by remember { mutableStateOf(10.dp) }
 
-        Column(modifier = Modifier.wrapContentSize()) {
-        var escuela by rememberSaveable { mutableStateOf("") }
-        var periodo by rememberSaveable { mutableStateOf("") }
-        var codigo by rememberSaveable { mutableStateOf("") }
-            var size by rememberSaveable { mutableStateOf("") }
-        //
-        Text(
+    var mExpanded by remember { mutableStateOf(false) }
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column(
             modifier = Modifier
-                .padding(16.dp, 0.dp)
-                .alpha(0.6f),
-            text = "EMAIL",
-            fontWeight = FontWeight.SemiBold,
-            color = Color.Gray,
-            fontSize = 12.sp
-        )
-        //email field
-        OutlinedTextField(
-            value = escuela,
-            onValueChange = { escuela = it },
+                .wrapContentSize()
+                .verticalScroll(rememberScrollState())
 
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType
-                = KeyboardType.Email
-            ),
-            modifier = Modifier
-                .padding(16.dp, 0.dp, 16.dp, 0.dp)
-                .fillMaxWidth(),
+    ) {
+            var escuela by rememberSaveable { mutableStateOf("") }
+            var periodo by rememberSaveable { mutableStateOf("") }
+            var codigo by rememberSaveable { mutableStateOf("") }
+            var nombre by rememberSaveable { mutableStateOf("") }
+            var semestre by rememberSaveable { mutableStateOf("") }
+            var duracion by rememberSaveable { mutableStateOf("") }
+            var size by rememberSaveable { mutableStateOf("0") }
 
-            )
-        Text(
-            modifier = Modifier
-                .padding(16.dp, 0.dp)
-                .alpha(0.6f),
-            text = "PERIODO",
-            fontWeight = FontWeight.SemiBold,
-            color = Color.Gray,
-            fontSize = 12.sp
-        )
-        //email field
-        OutlinedTextField(
-            value = periodo,
-            onValueChange = { periodo = it },
-
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType
-                = KeyboardType.Email
-            ),
-            modifier = Modifier
-                .padding(16.dp, 0.dp, 16.dp, 0.dp)
-                .fillMaxWidth(),
-
-            )
-        Text(
-            modifier = Modifier
-                .padding(16.dp, 0.dp)
-                .alpha(0.6f),
-            text = "Codigo",
-            fontWeight = FontWeight.SemiBold,
-            color = Color.Gray,
-            fontSize = 12.sp
-        )
-        //email field
-        OutlinedTextField(
-            value = codigo,
-            onValueChange = { codigo = it },
-
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType
-                = KeyboardType.Email
-            ),
-            modifier = Modifier
-                .padding(16.dp, 0.dp, 16.dp, 0.dp)
-                .fillMaxWidth(),
-
-            )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = {
-                //launch the class in a coroutine scope
-                scope.launch {
-                    dataStore.saveCodigo(codigo)
-                    dataStore.saveEscuela(escuela)
-                    dataStore.savePeriodo(periodo)
+            var textSize by rememberSaveable { mutableStateOf("0") }
+            scope.launch {
+                dataStore.getSize.collect { data ->
+                    textSize = data.toString()
                 }
+            }
 
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(60.dp)
-                .padding(16.dp, 0.dp, 16.dp, 0.dp),
-        ) {
-            Text(
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.White,
-                text = "Guardar Datos",
-
-                )
-        }
-
+            // Componente periodo
             Text(
                 modifier = Modifier
                     .padding(16.dp, 0.dp)
                     .alpha(0.6f),
-                text = "Tamaño ",
+                text = "PERIODO ",
                 fontWeight = FontWeight.SemiBold,
                 color = Color.Gray,
-                fontSize = 12.sp
+                fontSize = textSize.toInt().sp
             )
-            //email field
+
             OutlinedTextField(
-                value = size,
-                onValueChange = { size = it },
+                value = periodo,
+                onValueChange = { periodo = it },
+
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType
+                    = KeyboardType.Email
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
+            )
+
+
+            // Componente ESCUELA
+            Text(
+                modifier = Modifier
+                    .padding(16.dp, 0.dp)
+                    .alpha(0.6f),
+                text = "ESCUELA ",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray,
+                fontSize = textSize.toInt().sp
+            )
+
+            OutlinedTextField(
+                value = escuela,
+                onValueChange = { escuela= it },
 
                 keyboardOptions = KeyboardOptions.Default.copy(
                     keyboardType
@@ -169,53 +133,208 @@ fun LoginSCreen() {
                     .fillMaxWidth(),
 
                 )
-            Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = {
-                scope.launch {
-                    dataStore.saveSize(size as Float)
 
-                }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(60.dp)
-                .padding(16.dp, 0.dp, 16.dp, 0.dp),
-        ) {
+
+            // Componente CODIGO
             Text(
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.White,
-                text = "Aumentar Tamaño",
+                modifier = Modifier
+                    .padding(16.dp, 0.dp)
+                    .alpha(0.6f),
+                text = "Codigo",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray,
+                fontSize = textSize.toInt().sp
+            )
+
+            OutlinedTextField(
+                value = codigo,
+                onValueChange = { codigo = it },
+
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType
+                    = KeyboardType.Email
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
 
                 )
-        }
-        Spacer(modifier = Modifier.height(32.dp))
 
-        val userEmail = dataStore.getEscuela.collectAsState(initial = "")
-        val userC = dataStore.getCodigo.collectAsState(initial = "")
-        val userP = dataStore.getPeriodo.collectAsState(initial = "")
+            // Componente NOMBRE
+            Text(
+                modifier = Modifier
+                    .padding(16.dp, 0.dp)
+                    .alpha(0.6f),
+                text = "NOMBRE  ",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray,
+                fontSize = textSize.toInt().sp
+            )
 
-        Text(text = userEmail.value!!)
-        Text(text = userP.value!!)
-        Text(text = userC.value!!)
+            OutlinedTextField(
+                value = nombre,
+                onValueChange = { nombre = it },
+
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType
+                    = KeyboardType.Email
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
+            )
+            //COMPONENTE SEMESTRE
+            Text(
+                modifier = Modifier
+                    .padding(16.dp, 0.dp)
+                    .alpha(0.6f),
+                text = "SEMESTRE: ",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray,
+                fontSize = textSize.toInt().sp
+            )
+
+            OutlinedTextField(
+                value = semestre,
+                onValueChange = { semestre = it },
+
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType
+                    = KeyboardType.Text
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
+            )
+
+            //COMPONENET DURACION
+            Text(
+                modifier = Modifier
+                    .padding(16.dp, 0.dp)
+                    .alpha(0.6f),
+                text = "DURACION  ",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray,
+                fontSize = textSize.toInt().sp
+            )
+
+            OutlinedTextField(
+                value = duracion,
+                onValueChange = { duracion = it },
+
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType
+                    = KeyboardType.Email
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
+            )
 
 
-    }
-}
-@Composable
-fun Greeting(context: Context) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    //launch the class in a coroutine scope
+                    scope.launch {
+                        dataStore.saveCodigo(codigo)
+                        dataStore.saveEscuela(escuela)
+                        dataStore.savePeriodo(periodo)
+                        dataStore.saveNombre(nombre)
+                        dataStore.saveSemestre(semestre)
+                        dataStore.saveDuracion(duracion)
+                    }
 
-    Surface() {
-        Column() {
-            Button(onClick = { /*TODO*/ }) {
-
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
+            ) {
+                Text(
+                    style = MaterialTheme.typography.subtitle1,
+                    color = Color.White,
+                    text = "Guardar Datos",
+                    fontSize = textSize.toInt().sp
+                )
             }
-            Text("")
-            Button(onClick = { /*TODO*/ }) {
 
+            Text(
+                modifier = Modifier
+                    .padding(16.dp, 0.dp)
+                    .alpha(0.6f),
+                text = "Tamaño ",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Gray,
+                fontSize = textSize.toInt().sp
+            )
+            //email field
+            OutlinedTextField(
+
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType
+                    = KeyboardType.Number
+                ),
+
+                value = size,
+                onValueChange = { size = it },
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
+
+                )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    scope.launch {
+
+                        dataStore.saveSize(size.toFloat().toInt())
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
+            ) {
+                Text(
+                    style = MaterialTheme.typography.subtitle1,
+                    color = Color.White,
+                    text = "Cambiar tamaño de letra",
+                    fontSize = textSize.toInt().sp
+                )
             }
 
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            val userEscuela = dataStore.getEscuela.collectAsState(initial = "")
+            val userC = dataStore.getCodigo.collectAsState(initial = "")
+            val userP = dataStore.getPeriodo.collectAsState(initial = "")
+            val userNom = dataStore.getEscuela.collectAsState(initial = "")
+            val userSem = dataStore.getCodigo.collectAsState(initial = "")
+            val userDur = dataStore.getPeriodo.collectAsState(initial = "")
+            val userS = dataStore.getSize.collectAsState(initial = "")
+
+            Text(text = "ESCUELA :  ${userEscuela.value!!}")
+            Text(text = "PERIODO :  ${userP.value!!}")
+            Text(text = "CODIGO :  ${userC.value!!}")
+            Text(text = "NOMBRE :  ${userNom.value!!}")
+            Text(text = "SEMESTRE :  ${userSem.value!!}")
+            Text(text = "DURACION :  ${userDur.value!!}")
+
+            Text(text = "TAMAÑO DE LETRA ${userS.value!!}")
+
         }
-    }
+
 }
+
+
+
+
+
+
+
 
